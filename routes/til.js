b00baaa611b4b64648348c7307a9152a9fa30e28
haleynwheeler <haley.n.wheeler@gmail.com>
2016-03-07 19:47:59 -0700

    change in til.js

--- routes/til.js
+++ routes/til.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-var tilEntries = []
+var tilEntries = [
   {slug: "TIL: March 5, 2016", body: "stuff"},
   {slug: "TIL: March 5, 2016", body: "other stuff"}
 ];
